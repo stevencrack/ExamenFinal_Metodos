49aa4b643afe36950bc450dc776f56b4c4cb6272
stevencrack-examenfinal_metodos-5gsxf1es <stevencrack-examenfinal_metodos-5gsxf1es@local>
2018-05-25 01:02:01 +0000

    ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -8,5 +8,11 @@ import numpy as np
 x = np.linspace(0,2.,10)
 fx = np.array([0., 0.0494, 0.1975, 0.4444, 0.7901,1.2346 , 1.7778, 2.4198, 3.1605, 4.])
 
+def fderivada(x,fx)
+
+    second=()
+    return second
+
+print (fderivada(x,fx))
 
 
