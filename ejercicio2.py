a1ff4153afb335994b00c77677dfc41f930dbe22
stevencrack-examenfinal_metodos-5gsxf1es <stevencrack-examenfinal_metodos-5gsxf1es@local>
2018-05-25 01:06:16 +0000

    ejercicio2

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,8 +3,27 @@
 # los numeros impares y que pare de imprimir al encontrar un numero mayor a 800
 import numpy as np
 
+#n=0
+i=1
+a=800
+l=[]
 x = np.int_(np.random.random(100)*1000)
-print(x)
+
+for i in range(800):
+    if (x[i]<(a) and x[i]%2!=0):
+        l.append(x[i])
+        
+    elif(x[i]>800):
+        
+        break
+        
+print (l);
+
+#while(n<800):
+#    if n%2!=0:
+#        print (i)
+        
+#print(x)
 
 
 
